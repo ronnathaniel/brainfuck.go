ad1a4e7568a31a9410a1383b123355abaa3aa887
Ron Nathaniel <rnathaniel7@gmail.com>
2020-12-08 23:48:54 -0500

    command line args supported

--- cmd.go
+++ cmd.go
@@ -2,14 +2,41 @@
 package main
 
 import (
-	"path/filepath"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	inputRaw string
 )
 
 func main() {
+	//inputRaw = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+	//parse(inputRaw)
 
-
+	inputRaw = parseArgs()
+	Exec(inputRaw)
 }
 
-func openFile(path string) (string, error) {
-	return filepath.Abs(path)
+func parseArgs() string {
+	fmt.Println(os.Args)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("\tbrainfuck <path/to/file>.bf")
+		fmt.Println("\tbrainfuck --exec <bf logic>")
+	} else if len(os.Args) == 2 {
+		return OpenFile(os.Args[1])
+	} else if len(os.Args) == 3 {
+		return os.Args[2]
+	}
+
+	return ""
+}
+
+func OpenFile(path string) string {
+	f, err := ioutil.ReadFile(path)
+	check(err)
+
+	return string(f)
 }
